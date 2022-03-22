1fcb08555c9aa09c0f1853afe979bb670e4d21bc
prasunani <102089532+prasunani@users.noreply.github.com>
2022-03-22 13:35:37 -0500

    Update Test1.java

--- src/test/java/tests/Test1.java
+++ src/test/java/tests/Test1.java
@@ -6,7 +6,7 @@ public class Test1 {
 	{
 		// TODO Auto-generated method stub
 		int a=10;
-	     int b=20;
+	     int b=30;
 	     int c=a+b;
 	     System.out.println("addition of a and b is"+c);
 
